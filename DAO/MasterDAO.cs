6c592646538933c2177c1080faad06fa59ca0e82
corneliusanggawp <corneliuswidipratama@gmail.com>
2022-06-09 09:06:39 +0700

    (+) Adding Dynamic Menu

--- DAO/MasterDAO.cs
+++ DAO/MasterDAO.cs
@@ -8,31 +8,77 @@ namespace SISPRA.DAO
 {
     public class MasterDAO
     {
+        public List<dynamic> getAllMenu(Array id_role)
+        {
+            using (SqlConnection conn = new SqlConnection(DBConnection.db_sispras))
+            {
+                try
+                {
+                    string query = @"
+                        SELECT      ID_SI_MENU, ID_SISTEM_INFORMASI, DESKRIPSI, ISACTIVE, LINK, NO_URUT, ICON
+                        FROM        siatmax.TBL_SI_MENU
+                        WHERE       ID_SISTEM_INFORMASI = 2 AND ISACTIVE = 1 ";
+
+                    var data = conn.Query<dynamic>(query).ToList();
+
+                    return data;
+                }
+                catch (Exception ex)
+                {
+                    return new List<dynamic>();
+                }
+                finally
+                {
+                    conn.Dispose();
+                }
+            }
+        }
+
         public List<dynamic> getAllSubMenu(Array id_role)
         {
             using (SqlConnection conn = new SqlConnection(DBConnection.db_sispras))
             {
                 try
                 {
-                    int first = 0;
+                    int arrayIndex = 0;
 
                     string query = @"
                         SELECT      siatmax.TBL_ROLE_SUBMENU.ID_SI_SUBMENU, siatmax.TBL_ROLE_SUBMENU.ID_ROLE, siatmax.TBL_SI_SUBMENU.ID_SI_MENU, siatmax.TBL_SI_SUBMENU.DESKRIPSI, siatmax.TBL_SI_SUBMENU.ISACTIVE, siatmax.TBL_SI_SUBMENU.LINK
                         FROM        siatmax.TBL_ROLE_SUBMENU 
                         INNER JOIN  siatmax.TBL_SI_SUBMENU ON siatmax.TBL_ROLE_SUBMENU.ID_SI_SUBMENU = siatmax.TBL_SI_SUBMENU.ID_SI_SUBMENU
-                        WHERE       siatmax.TBL_SI_SUBMENU.ISACTIVE = 1
-                        AND";
+                        INNER JOIN  siatmax.TBL_SI_MENU ON siatmax.TBL_SI_SUBMENU.ID_SI_MENU = siatmax.TBL_SI_MENU.ID_SI_MENU
+                        WHERE       siatmax.TBL_SI_MENU.ID_SISTEM_INFORMASI = 2 AND siatmax.TBL_SI_SUBMENU.ISACTIVE = 1";
 
                     if (Array.IndexOf(id_role, "9") != -1)
                     {
-                        query += @"siatmax.TBL_ROLE_SUBMENU.ID_ROLE = 9";
+                        query += @"AND siatmax.TBL_ROLE_SUBMENU.ID_ROLE = 9";
                     }
                     else
                     {
-                        foreach (var id in id_role)
+                        if(id_role.Length > 1)
                         {
-                            first = 1;
-                            query += @" AND siatmax.TBL_ROLE_SUBMENU.ID_ROLE " + String.Join("=", id);
+                            foreach (var id in id_role)
+                            {
+                                arrayIndex++;
+                                if (arrayIndex > 1)
+                                {
+                                    query += @" OR ";
+                                }
+                                else
+                                {
+                                    query += @" AND (";
+                                }
+
+                                query += @" siatmax.TBL_ROLE_SUBMENU.ID_ROLE = " + String.Join("", id);
+                            }
+                            query += @")";
+                        }
+                        else
+                        {
+                            foreach (var id in id_role)
+                            {
+                                query += @" AND siatmax.TBL_ROLE_SUBMENU.ID_ROLE = " + String.Join("", id);
+                            }
                         }
                     }
 
@@ -60,6 +106,7 @@ namespace SISPRA.DAO
                     string query = @"
                         SELECT *
                         FROM siatmax.MST_UNIT";
+
                     var data = conn.Query<dynamic>(query).ToList();
 
                     return data;
