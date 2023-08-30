79e47e4fa8256ae952db3e9b4d701e1508e1a4f0
Cornelius Angga Widiprasetyo <66959246+corneliusanggawp@users.noreply.github.com>
2023-08-31 01:36:43 +0700

    Delete DAO/DBConnection.cs

--- DAO/DBConnection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SISPRAS.DAO
-{
-    public class DBConnection
-    {
-        public static string db_sispras = @"Server=192.168.15.156;Database=SIATMAX_SISPRAS;User Id=sispras1;Password=sispras123!";
-        //public static string db_sispras = @"Server=202.14.92.208;Database=SIATMAX_SISPRAS;User Id=sispras1;Password=sispras123!";
-    }
-}
